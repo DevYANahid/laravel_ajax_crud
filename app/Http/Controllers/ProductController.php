31c6ada2dcef68151e5db8a5fb44b63cdba0af1b
DevYANahid <codealgorithmschool@gmail.com>
2024-10-02 22:38:25 +0600

    init

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::orderby('created_at')->get();
+        return view('products.index',['products'=>$products]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product;
+
+        $file_name = time(). '.' . request()->image->getClientOriginalExtension();
+        request()->image->move(public_path('image'), $file_name);
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->image = $file_name;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+
+
+        $product->save();
+        return redirect()->route('product.index')->with('success' ,'Product Added Successfully');
+    }
+}
